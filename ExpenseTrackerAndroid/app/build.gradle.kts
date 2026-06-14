f6b992a8b35451342264ebc0faa7ee814e737c51
aitran <tcai2000ely@gmail.com>
2026-06-14 19:59:13 +0700

    + init source

--- ExpenseTrackerAndroid/app/build.gradle.kts
+++ ExpenseTrackerAndroid/app/build.gradle.kts
@@ -6,15 +6,13 @@ plugins {
 android {
     namespace = "com.aitc.expensetrackerandroid"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.aitc.expensetrackerandroid"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
